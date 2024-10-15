dfc81001ac0a09dbd9c59730b38ef6aadcbdf4c8
Riccardo Foti <riccardofoti97@gmail.com>
2024-10-15 17:21:39 +0100

    Add error handling

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const csv = require("csv-parser");
 const fs = require("fs");
-const path = "hb_test.csv";
 const ObjectsToCsv = require("objects-to-csv");
+
+const inputFilePath = "hb_test.csv";
+const outputFilePath = "output.csv";
 
 let products = [];
 
-fs.createReadStream(path)
+if (!fs.existsSync(inputFilePath)) {
+  throw new Error(`Error: ${inputFilePath} does not exist`);
+}
+
+fs.createReadStream(inputFilePath)
   .pipe(csv())
   .on("data", (row) => {
     const [bay, shelf] = row.pick_location.split(" ");
@@ -17,59 +23,77 @@ fs.createReadStream(path)
     });
   })
   .on("end", () => {
+    if (products.length === 0) {
+      throw new Error(`Error: there are no products to process`);
+    }
+
     const aggregatedProducts = aggregateProducts(products);
     const sortedProducts = sortProducts(aggregatedProducts);
     writeDataToCsv(sortedProducts);
+  })
+  .on("error", (error) => {
+    throw new Error(`Error reading CSV file: ${error}`);
   });
 
 function aggregateProducts(products) {
-  const aggregatedProducts = [];
-  const aggregatedProductsMap = {};
+  try {
+    const aggregatedProducts = [];
+    const aggregatedProductsMap = {};
 
-  for (const product of products) {
-    const id = product["product_code"];
-    // If the product already exists increase its quantity
-    if (aggregatedProductsMap[id]) {
-      aggregatedProductsMap[id].quantity += product.quantity;
-    } else {
-      aggregatedProductsMap[id] = { ...product };
+    for (const product of products) {
+      const id = product["product_code"];
+      // If the product already exists increase its quantity
+      if (aggregatedProductsMap[id]) {
+        aggregatedProductsMap[id].quantity += product.quantity;
+      } else {
+        aggregatedProductsMap[id] = { ...product };
+      }
     }
+    for (const id in aggregatedProductsMap) {
+      aggregatedProducts.push(aggregatedProductsMap[id]);
+    }
+    return aggregatedProducts;
+  } catch (error) {
+    throw new Error(`Error aggregating products : ${error}`);
   }
-  for (const id in aggregatedProductsMap) {
-    aggregatedProducts.push(aggregatedProductsMap[id]);
-  }
-  return aggregatedProducts;
 }
 
 function sortProducts(products) {
-  products.sort((a, b) => {
-    // if bays are the same, order by shelf number
-    if (a.bay.localeCompare(b.bay) === 0) {
-      return a.shelf - b.shelf;
-    }
-    // if bays have the same amount of characters, order alphabetically
-    if (a.bay.length === b.bay.length) {
-      return a.bay.localeCompare(b.bay);
-    }
-    // order by bays with less characters first
-    return a.bay.length - b.bay.length;
-  });
+  try {
+    products.sort((a, b) => {
+      // if bays are the same, order by shelf number
+      if (a.bay.localeCompare(b.bay) === 0) {
+        return a.shelf - b.shelf;
+      }
+      // if bays have the same amount of characters, order alphabetically
+      if (a.bay.length === b.bay.length) {
+        return a.bay.localeCompare(b.bay);
+      }
+      // order by bays with less characters first
+      return a.bay.length - b.bay.length;
+    });
 
-  return products;
+    return products;
+  } catch (error) {
+    throw new Error(`Error sorting products: ${error}`);
+  }
 }
 
 async function writeDataToCsv(products) {
-  // format data for CSV output
-  const data = products.map((product) => ({
-    product_code: product.product_code,
-    quantity: product.quantity,
-    pick_location: `${product.bay} ${product.shelf}`,
-  }));
+  try {
+    // format data for CSV output
+    const data = products.map((product) => ({
+      product_code: product.product_code,
+      quantity: product.quantity,
+      pick_location: `${product.bay} ${product.shelf}`,
+    }));
 
-  const outputFilename = "output.csv";
-  const objToCsv = new ObjectsToCsv(data);
-  await objToCsv.toDisk(`./${outputFilename}`);
-  console.log(
-    `Your products have been successfully sorted into ${outputFilename}!`
-  );
+    const objToCsv = new ObjectsToCsv(data);
+    await objToCsv.toDisk(`./${outputFilePath}`);
+    console.log(
+      `Your products have been successfully sorted into ${outputFilePath}!`
+    );
+  } catch (error) {
+    throw new Error(`Error writing data to CSV: ${error}`);
+  }
 }
