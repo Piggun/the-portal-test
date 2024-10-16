9fe2241c447e5f7ba07ef86c9e8c3d7d07e8bee1
Riccardo Foti <riccardofoti97@gmail.com>
2024-10-16 16:10:32 +0100

    Move utility functions into utils.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const ObjectsToCsv = require("objects-to-csv");
+const { aggregateProducts, sortProducts } = require("./utils.js");
 
 const inputFilePath = "hb_test.csv";
 const outputFilePath = "output.csv";
@@ -35,49 +36,6 @@ function processCsv() {
     .on("error", (error) => {
       throw new Error(`Error reading CSV file: ${error}`);
     });
-}
-
-function aggregateProducts(products) {
-  try {
-    const aggregatedProducts = [];
-    const aggregatedProductsMap = {};
-    for (const product of products) {
-      const id = product["product_code"];
-      // If the product already exists increase its quantity
-      if (aggregatedProductsMap[id]) {
-        aggregatedProductsMap[id].quantity += product.quantity;
-      } else {
-        aggregatedProductsMap[id] = { ...product };
-      }
-    }
-    for (const id in aggregatedProductsMap) {
-      aggregatedProducts.push(aggregatedProductsMap[id]);
-    }
-    return aggregatedProducts;
-  } catch (error) {
-    throw new Error(`Error aggregating products: ${error}`);
-  }
-}
-
-function sortProducts(products) {
-  try {
-    products.sort((a, b) => {
-      // if bays are the same, order by shelf number
-      if (a.bay.localeCompare(b.bay) === 0) {
-        return a.shelf - b.shelf;
-      }
-      // if bays have the same amount of characters, order alphabetically
-      if (a.bay.length === b.bay.length) {
-        return a.bay.localeCompare(b.bay);
-      }
-      // order by bays with less characters first
-      return a.bay.length - b.bay.length;
-    });
-
-    return products;
-  } catch (error) {
-    throw new Error(`Error sorting products: ${error}`);
-  }
 }
 
 async function writeDataToCsv(products) {
